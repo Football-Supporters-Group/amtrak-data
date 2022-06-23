a5fccb0463912a5d19f1b235c72bfb18e75fe728
Mark Wolgin <wolginmark@gmail.com>
2022-06-22 22:57:10 -0400

    Adding constants for routes

---
+++ src/main/java/com/wolginm/amtrak/data/models/constants/ConsolidatedRouteLists.java
@@ -0,0 +1,31 @@
+package com.wolginm.amtrak.data.models.constants;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ConsolidatedRouteLists {
+
+    private final List<String> adirondackTrailways = Arrays.asList(new String[]{"NYP", "YNY", "CRT", "POU",
+        "RHI", "HUD", "ALB", "SDY", "SAR", "FED", "WHL", "FTC", "POH", "WSP", "PRK", "PLB", "RSP", 
+        "SLQ", "MTR"});
+
+    private final List<String> keystoneService = Arrays.asList(new String[]{""});
+    
+    public List<String> getStationList(int routeId) {
+        List<String> selectedRoute;
+
+        switch(routeId) {
+            case 94:
+                selectedRoute = this.keystoneService;
+                break;
+            case 95:
+                selectedRoute = this.adirondackTrailways;
+                break;
+            default:
+                selectedRoute = null;
+                break;
+        }
+
+        return selectedRoute;
+    }
+}

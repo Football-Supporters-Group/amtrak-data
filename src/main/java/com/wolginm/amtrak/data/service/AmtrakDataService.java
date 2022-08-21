c6aed76a4d5683360f8d41fd853985f3c840e329
Mark Wolgin <wolginmark@gmail.com>
2022-08-21 11:46:04 -0400

    issue-2 - adding more routes

--- src/main/java/com/wolginm/amtrak/data/service/AmtrakDataService.java
+++ src/main/java/com/wolginm/amtrak/data/service/AmtrakDataService.java
@@ -7,21 +7,24 @@ import com.wolginm.amtrak.data.models.consolidated.ConsolidatedRoute;
 import com.wolginm.amtrak.data.models.consolidated.Trip;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AmtrakDataService {
     
     private AmtrakDataHandler amtrakDataHandler;
+    private boolean rePullFiles;
 
     @Autowired
-    public AmtrakDataService(AmtrakDataHandler amtrakDataHandler) {
+    public AmtrakDataService(AmtrakDataHandler amtrakDataHandler, @Qualifier("NeedToPullData") boolean rePullFiles) {
         this.amtrakDataHandler = amtrakDataHandler;
+        this.rePullFiles = rePullFiles;
         this.prepDataForUse();
     }
 
     public void prepDataForUse() {
-        this.amtrakDataHandler.updateAmtrakDataFiles(true);
+        this.amtrakDataHandler.updateAmtrakDataFiles(this.rePullFiles);
         this.amtrakDataHandler.loadFilesToMemory();
         this.amtrakDataHandler.compileTrips();
         this.amtrakDataHandler.compileRoutes();
